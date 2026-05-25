d0a5a2731346a1497151cbf987b3dcc216667634
BaoHan1712 <hanquocbao1712@gmail.com>
2026-05-25 19:19:33 +0700

    main

--- main19_05_26See_KFS1_2L/main/cover/convert.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO(r"cover/models/nhathop.pt")  # load an official model
-# # Export the model
-model.export(format="onnx", half = True)
-# results = model.predict(r"cover\do3.mp4",  show=True, conf=0.4, imgsz= 640)  
--- main19_05_26See_KFS1_2L/main/cover/show_class.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-# Load a pretrained YOLO11n model
-model = YOLO(r"cover/models/gaphop.onnx")
-
-print(model.names) 
